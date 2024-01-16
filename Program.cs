9a8b918e4640fc2cb67607773d63d7ef5a7655c4
thien <ctthien304@gmail.com>
2024-01-16 19:40:49 +0700

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,42 @@
 ﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
 
-abstract class Product{
-    
+public abstract class Product{
+    protected int productId;
+    protected string name;
+    protected double price;
+    protected String Producer;
+
+    public Product(int productId, String name, double price, String Producer){
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.Producer = Producer;
+    }
+    public abstract double ComputeTax();
+}
+public class Book : Product{
+    public override double ComputeTax()
+    {
+      return price * 0.08;
+    }
+}
+
+class Mobilephone: Product{
+
+    public override double ComputeTax()
+    {
+        return price *0.1;
+    }
 
 }
+  class Program{
+    static void Main(String[]args){
+        Mobilephone phone = new Mobilephone();
+        phone.Price = 1000;
+
+        double tax = phone.ComputeTax();
+        System.Console.WriteLine("tax for the mobile phone: " + tax);
+    }
+  }
